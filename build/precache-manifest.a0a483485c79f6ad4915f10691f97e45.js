2de65cf12507827290246271c542d84514fbf30f
frazled <info@pipsltd.com>
2022-04-10 18:29:40 +0100

    Add files via upload

---
+++ build/precache-manifest.a0a483485c79f6ad4915f10691f97e45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10395891abf05ff91a7be6a5e341810",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45222804ca2f20149259",
+    "url": "/static/css/main.9136e423.chunk.css"
+  },
+  {
+    "revision": "b78bd3b6073e08564c76",
+    "url": "/static/js/2.c3a2d25b.chunk.js"
+  },
+  {
+    "revision": "45ca725996a86fc7b774ef915f1c0993",
+    "url": "/static/js/2.c3a2d25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6070c66c97f163c841",
+    "url": "/static/js/3.ee76a521.chunk.js"
+  },
+  {
+    "revision": "45222804ca2f20149259",
+    "url": "/static/js/main.6c7b90b8.chunk.js"
+  },
+  {
+    "revision": "aa194623e7f3315b810d",
+    "url": "/static/js/runtime-main.c1bb6951.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  }
+]);
